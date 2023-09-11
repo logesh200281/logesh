0d1ccb4fec715028624a830c79bac4f05aaf3da9
logesh200281 <2001211036119@sankaracollege.edu.in>
2023-09-11 13:45:07 +0530

    Your commit message here

--- src/Home.js
+++ src/Home.js
@@ -1,65 +1,79 @@
 import React, { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "./Home.css";
-import logo from './LOGO.png'; // Import the logo image using the correct path
-import image12 from './12.jpg'; // Import the second image using the correct path
-import image13 from './naac-img.png'; // Import the third image using the correct path
-import image14 from './iso-img.png'; // Import the fourth image using the correct path
+import logo from './LOGO.png';
+import image12 from './12.jpg';
+import image13 from './naac-img.png';
+import image14 from './iso-img.png';
+import ImageSlider from "./imageslider";
 
 function Navbar() {
-    const navRef = useRef();
+  const navRef = useRef();
 
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
 
-    return (
-        <header className="header">
-            <nav ref={navRef}>
-            <a href="./Home.js">Home</a>
-          <a href="/about">About</a>
-          <a href="/services">Department</a>
-          <a href="/portfolio">COURSES</a>
-          <a href="/contact">ADMISSION</a>
-          <a href="/contact">ACTIVITIES</a>
-          <a href="/contact">ACADEMICS</a>
-          <a href="/contact">MEDIA</a>
-          <a href="/contact">NIRF</a>
-          <a href="/contact">GALLERY</a>
-          <a href="/contact">CONTACT US</a>
-                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                       <FaTimes/>
-                </button>
-            </nav>
-            <button className="nav-btn" onClick={showNavbar}>
-                <FaBars/>
-            </button>
-        </header>
-    );
+  return (
+    <header className="header">
+      <nav ref={navRef}>
+        <a href="./Home.js">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Department</a>
+        <a href="/portfolio">COURSES</a>
+        <a href="/contact">ADMISSION</a>
+        <a href="/contact">ACTIVITIES</a>
+        <a href="/contact">ACADEMICS</a>
+        <a href="/contact">MEDIA</a>
+        <a href="/contact">NIRF</a>
+        <a href="/contact">GALLERY</a>
+        <a href="/contact">CONTACT US</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes/>
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars/>
+      </button>
+    </header>
+  );
 }
 
 function Hero() {
-    return (
-        <div className="hero">
-            <div className="image-container">
-                <img src={logo} alt="Logo" className='logo' /> {/* Use the imported logo */}
-                <img src={image13} alt="Image 13" className='logo' /> {/* Use the imported image */}
-                <img src={image14} alt="Image 14" className='logo' /> {/* Use the imported image */}
-                <img src={image12} alt="Image 12" className='logo' /> {/* Use the imported image */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="hero">
+      <div className="image-container">
+        <img src={logo} alt="Logo" className='logo' />
+        <img src={image13} alt="Image 13" className='logo' />
+        <img src={image14} alt="Image 14" className='logo' />
+        <img src={image12} alt="Image 12" className='logo' />
+      </div>
+    </div>
+  );
 }
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar /> {/* Include the Navbar component */}
-            <Hero />
-            
-            <ImageSlider /> {/* Correctly use the ImageSlider component */}
+  const isMobile = window.innerWidth <= 768; // Define your breakpoint for mobile view
+
+  return (
+    <div className="App">
+      <Navbar />
+      {isMobile ? (
+        // Content to display on mobile view
+        <div>
+          <h1>Mobile View Content</h1>
+          {/* Add mobile-specific content here */}
         </div>
-    );
+      ) : (
+        // Content to display on web view
+        <div>
+          <Hero />
+          <ImageSlider />
+          {/* Add web-specific content here */}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/imageslider.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
+// Import your local images
+import image1 from './ass/banner.jpg.crdownload';
+import image2 from './ass/banner4.jpg.crdownload';
+import image3 from './ass/banner5.jpg.crdownload';
+import image4 from './ass/IMG-20210301-WA0002-1-new.jpg.crdownload';
+
+const slideshow = [
+    {
+        URL: image1,
+        caption: 'Caption 1'
+    },
+    {
+        URL: image2,
+        caption: 'Caption 2'
+    },
+    {
+        URL: image3,
+        caption: 'Caption 3'
+    },
+    {
+        URL: image4,
+        caption: 'Caption 4'
+    },
+];
+
+const divstyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '650px',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    transition: 'transform 0.2s ease-in-out', // Add the transition for transform
+};
+
+const spanstyle = {
+    fontSize: '20px',
+    background: '#efefef',
+    color: '#000000',
+};
+
+function imageslider() {
+    return (
+        <div className='slide-container'>
+            <Fade>
+                {slideshow.map((image, index) => (
+                    <div key={index}>
+                        <div style={{ ...divstyle, backgroundImage: `url(${image.URL})` }}></div>
+                        <span style={spanstyle}>{image.caption}</span>
+                    </div>
+                ))}
+            </Fade>
+        </div>
+    );
+}
+
+export default imageslider;
